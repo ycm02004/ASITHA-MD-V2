4e828422903fc8622d448822654db6e9d4319bae
ycm02004 <christianmyguel240@gmail.com>
2024-12-21 10:46:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=265DhDqY#C33JP_0Ip0yjFq8cULTU9WOKYyp6v8Z_v7chtPR2DHU === undefined ? '' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
